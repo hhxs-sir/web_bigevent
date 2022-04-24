5544d7a86c6afe3291979fa664e54d4665f6c495
hhxs <644947740@qq.com>
2022-04-24 18:34:10 +0800

    更新了

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,5 +7,14 @@ $(function(){
         $('.login-box').show(1000);
         $('.reg-box').hide();  
       })
-
+    var form=layui.form;
+    form.verify({
+      pwd:[/^[\S]{6,12}$/,'密码必须6到12位，不能出现空格'],
+      repwd:function(value){
+          var pwd=$('.reg-box [name=password]').val();
+          if(pwd!==value){
+              return '两次密码不一样'
+          }
+      }
+    })
 })
